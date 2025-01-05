b03d8dab1d3a03560d61d646d1210bd7fbc1d608
Vince <vince@ultrabanana.net>
2025-01-05 16:43:14 +0100

    Added script for building regnskab

---
+++ regnskab/build_regnskab.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+from os import listdir;
+from os.path import isfile, isdir, join;
+from textwrap import indent;
+
+def walk(folder: str) -> str:
+    output: str = "";
+    output += "<ul>\n";
+    entries = listdir(folder);
+    entries.sort(reverse=True);
+    for entry in entries:
+        path = join(folder, entry);
+        if isfile(path):
+            output += f"\t<li><a href=\"{path}\">{entry}</a></li>\n";
+        elif isdir(path):
+            output += f"\t<li>{entry}:\n";
+            output += indent(walk(path), '\t\t');
+            output += "\t</li>\n";
+        else:
+            raise ValueError(f"Entry \"{entry}\" at \"{path}\" is neither a file nor directory");
+    output += "</ul>\n";
+    return output;
+            
+        
+
+print('''
+<!DOCTYPE html>
+<html lang="da">
+	<head>
+		<link rel="stylesheet" href="../base.css"/>
+		<link rel="icon" href="../logo.png"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8">
+		<title>ADSLs regnskab</title>
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li><a href=".."><img class="logo" src="../logo.png"></a></li>
+				<li><a href="../regnskab">Regnskab</a></li>
+				<li><a href="../vedtægter">Vedtægter</a></li>
+				<li><a href="../referat">Referater</a></li>
+				<li><p>lasaga</p></li>
+				<li><a href="../ADSL_Ansøgningsskema.docx">Søg om penge</a></li>
+			</ul>
+		</nav>
+		<main>
+			<h1>ADSLs Regnskab</h1>
+			<p>ADSL har politik om 100% åbent regnskab.</p>
+			<p>Regnskabet bliver presenteret til hver generelforsamling, og I kan finde de tidligere her:</p>
+''');
+
+print(indent(walk("documents"), '\t'*3));
+
+print('''
+		</main>
+	</body>
+</html>
+''');
